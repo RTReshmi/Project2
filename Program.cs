cf3cef5371f76546fc08bd5d5322c8f9fa0739ed
Ashiq Nazar <khanashiq18@gmail.com>
2022-05-04 09:46:07 +0530

    try catch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b, div;
+                Console.WriteLine("enter the first number");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter second number");
+                b = Convert.ToInt32(Console.ReadLine());
+                div = a / b;
+                Console.WriteLine(div);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("second number should not be zero.please try again");
+            }
+        }
+    }
+}
